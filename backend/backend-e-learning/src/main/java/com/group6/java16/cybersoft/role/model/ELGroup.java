eb4258906b946a5d60f9e0f977d140ccb7915e67
Nam Nguyen <nam03031999@gmail.com>
2022-04-22 04:50:47 -0400

    add table group

---
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/role/model/ELGroup.java
@@ -0,0 +1,22 @@
+package com.group6.java16.cybersoft.role.model;
+
+import javax.persistence.*;
+import com.group6.java16.cybersoft.common.model.BaseEntity;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "el_group")
+public class ELGroup  extends BaseEntity {
+    
+    private String name;
+    private String description;
+
+
+}
+
