3fa8908edc3f19b46670b3a7285ee32998f574cf
Trung Hậu <crud5204@gmail.com>
2022-04-26 19:03:37 +0700

    group

---
+++ backend/backend-e-learning/src/main/java/com/group6/java16/cybersoft/role/controller/GroupController.java
@@ -0,0 +1,13 @@
+package com.group6.java16.cybersoft.role.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/groups")
+@CrossOrigin(origins = "*")
+
+public class GroupController {
+
+}
